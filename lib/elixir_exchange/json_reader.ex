62777ed24a7dbfedda55c9bd5a8e0d1a6593e50a
bank8426 <bank_55555@hotmail.com>
2019-07-25 14:24:49 +0700

    update json_reader to return json body when succes

--- lib/elixir_exchange/json_reader.ex
+++ lib/elixir_exchange/json_reader.ex
@@ -1,21 +1,20 @@
 defmodule ElixirExchange.JSONReader do
 
   @doc """
-  This function will take path to a JSON file and parse string to JSON
+  This function will take path to a JSON file and parse JSON string to Maps
   iex> ElixirExchange.JSONReader.get_json("priv/input_1.json")
-  {:ok,
-   %{
-   "orders" => [
-     %{"amount" => 2.4, "command" => "sell", "price" => 100.003},
-     %{"amount" => 3.445, "command" => "buy", "price" => 90.394}
-   ]
-  }}
+  %{
+    "orders" => [
+      %{"amount" => 2.4, "command" => "sell", "price" => 100.003},
+      %{"amount" => 3.445, "command" => "buy", "price" => 90.394}
+    ]
+  }
   """
   def get_json(path) do
     with {:ok, body} <- File.read(path),
          {:ok, json} <- Poison.decode(body)
     do
-      {:ok, json}
+      json
     else
       err -> err
     end

2478eb262a13f9edadd6541a6e5f3f81ccdbc76a
bank8426 <bank_55555@hotmail.com>
2019-07-25 16:23:00 +0700

    add helper module for sorting by price

---
+++ lib/elixir_exchange/helper.ex
@@ -0,0 +1,55 @@
+defmodule ElixirExchange.Helper do
+  @moduledoc """
+  Provides a function `sort_price_ascending/1` to sort list by price with ascending order
+  Provides a function `sort_price_descending/1` to sort list by price with descending order
+  """
+
+  @doc """
+  sort list by price from low to high
+  ## Parameters
+    - list: list of unsorted orders
+  ## Examples
+    iex> data = [
+    ...>%{price: 90.394, volume: 3.445},
+    ...>%{price: 89.394, volume: 4.3},
+    ...>%{price: 90.15, volume: 1.305}
+    ...>]
+    iex>ElixirExchange.Helper.sort_price_ascending(data)
+    [
+      %{price: 89.394, volume: 4.3},
+      %{price: 90.15, volume: 1.305},
+      %{price: 90.394, volume: 3.445}
+    ]
+  """
+  def sort_price_ascending(list) do
+    list
+    |>Enum.sort_by(&(price(&1)))
+  end
+
+  @doc """
+  sort list by price from high to low
+  ## Parameters
+    - list: list of unsorted orders
+  ## Examples
+    iex> data = [
+    ...> %{price: 90.394, volume: 3.445},
+    ...> %{price: 89.394, volume: 4.3},
+    ...> %{price: 90.15, volume: 1.305}
+    ...> ]
+    iex>ElixirExchange.Helper.sort_price_descending(data)
+    [
+      %{price: 90.394, volume: 3.445},
+      %{price: 90.15, volume: 1.305},
+      %{price: 89.394, volume: 4.3}
+    ]
+  """
+  def sort_price_descending(list) do
+    list
+    |>Enum.sort_by(&(-price(&1)))
+  end
+
+  #get price from order
+  defp price(order) do
+    order[:price]
+  end
+end
---
+++ test/elixir_exchange/helper_test.exs
@@ -0,0 +1,50 @@
+defmodule HelperTest do
+  use ExUnit.Case
+  import ElixirExchange.Helper
+
+  doctest ElixirExchange.Helper
+
+  test "Passing list should sort ascending by price" do
+    mock_list = [
+      %{price: 90.394, volume: 3.445},
+      %{price: 89.394, volume: 4.3},
+      %{price: 90.15, volume: 1.305}
+    ]
+    expected_list = [
+      %{price: 89.394, volume: 4.3},
+      %{price: 90.15, volume: 1.305},
+      %{price: 90.394, volume: 3.445}
+    ]
+    ordered_list = ElixirExchange.Helper.sort_price_ascending(mock_list)
+    assert ordered_list == expected_list
+  end
+
+  test "Passing list should sort descending by price" do
+    mock_list = [
+      %{price: 90.394, volume: 3.445},
+      %{price: 89.394, volume: 4.3},
+      %{price: 90.15, volume: 1.305}
+    ]
+    expected_list = [
+      %{price: 90.394, volume: 3.445},
+      %{price: 90.15, volume: 1.305},
+      %{price: 89.394, volume: 4.3}
+    ]
+    ordered_list = ElixirExchange.Helper.sort_price_descending(mock_list)
+    assert ordered_list == expected_list
+  end
+
+  test "Passing empty list to sort_price_ascending should return empty list" do
+    mock_list = []
+    expected_list = []
+    ordered_list = ElixirExchange.Helper.sort_price_ascending(mock_list)
+    assert ordered_list == expected_list
+  end
+
+  test "Passing empty list to sort_price_descending should return empty list" do
+    mock_list = []
+    expected_list = []
+    ordered_list = ElixirExchange.Helper.sort_price_descending(mock_list)
+    assert ordered_list == expected_list
+  end
+end
